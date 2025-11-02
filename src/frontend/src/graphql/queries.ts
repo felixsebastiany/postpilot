0da229d90ba9a3b719c0d2a2486704c033b89fdd
Félix Hoffmann Sebastiany <felixsebastiany@grpereira.com.br>
2025-11-02 01:06:41 -0400

    feat: implementação de assinaturas Stripe com checkout redirect e gerenciamento de assinaturas
    - Integração com Stripe Checkout para assinaturas via redirect
    - GraphQL resolvers para gerenciamento de assinaturas (criar, cancelar, upgrade)
    - Fluxo completo de checkout usando mutations padrão do Magento
    - Componentes React para planos de assinatura e gerenciamento
    - Template de email customizado para invoices
    - Suporte para upgrade de plano Basic para Pro
    - Configuração de webhooks para processar pagamentos
    
    Módulos:
    - PostPilot/StripeCustom: integração customizada com Stripe
    - PostPilot/EmailTemplates: template de invoice

--- src/frontend/src/graphql/queries.ts
+++ src/frontend/src/graphql/queries.ts
@@ -177,3 +177,287 @@ export const RESET_PASSWORD = gql`
     resetPassword(email: $email, resetPasswordToken: $resetPasswordToken, newPassword: $newPassword)
   }
 `;
+
+// Subscription Queries
+export const GET_SUBSCRIPTION_PLANS = gql`
+  query GetSubscriptionPlans {
+    getSubscriptionPlans {
+      id
+      name
+      sku
+      price
+      currency
+      billing_period
+      description
+      features
+      is_popular
+    }
+  }
+`;
+
+export const GET_CUSTOMER_SUBSCRIPTION = gql`
+  query GetCustomerSubscription {
+    getCustomerSubscription {
+      id
+      status
+      plan_name
+      current_period_start
+      current_period_end
+      amount
+      currency
+      billing_period
+      cancel_at_period_end
+      payment_method {
+        id
+        type
+        card {
+          brand
+          last4
+          exp_month
+          exp_year
+        }
+      }
+    }
+  }
+`;
+
+export const CREATE_SUBSCRIPTION = gql`
+  mutation CreateSubscription($input: CreateSubscriptionInput!) {
+    createSubscription(input: $input) {
+      id
+      status
+      plan_name
+      current_period_start
+      current_period_end
+      amount
+      currency
+      billing_period
+      cancel_at_period_end
+      payment_method {
+        id
+        type
+        brand
+        exp_month
+        exp_year
+        label
+        icon
+      }
+    }
+  }
+`;
+
+export const GET_STRIPE_CONFIGURATION = gql`
+  query GetStripeConfiguration {
+    getStripeConfiguration {
+      apiKey
+      locale
+      appInfo
+      options {
+        betas
+        apiVersion
+      }
+      elementsOptions
+    }
+  }
+`;
+
+export const CUSTOMER_CART = gql`
+  query CustomerCart {
+    customerCart {
+      id
+      items {
+        id
+        quantity
+        product {
+          sku
+        }
+        prices {
+          price {
+            value
+            currency
+          }
+          row_total {
+            value
+            currency
+          }
+        }
+      }
+      prices {
+        grand_total {
+          value
+          currency
+        }
+      }
+      available_payment_methods {
+        code
+        title
+      }
+    }
+  }
+`;
+
+export const ADD_SIMPLE_PRODUCTS_TO_CART = gql`
+  mutation AddSimpleProductsToCart($input: AddSimpleProductsToCartInput!) {
+    addSimpleProductsToCart(input: $input) {
+      cart {
+        id
+        items {
+          id
+          quantity
+          product {
+            sku
+          }
+          prices {
+            price {
+              value
+              currency
+            }
+            row_total {
+              value
+              currency
+            }
+          }
+        }
+        prices {
+          grand_total {
+            value
+            currency
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const SET_BILLING_ADDRESS_ON_CART = gql`
+  mutation SetBillingAddressOnCart($input: SetBillingAddressOnCartInput!) {
+    setBillingAddressOnCart(input: $input) {
+      cart {
+        id
+        billing_address {
+          firstname
+          lastname
+          street
+          city
+          region {
+            code
+            label
+          }
+          postcode
+          country {
+            code
+            label
+          }
+          telephone
+        }
+      }
+    }
+  }
+`;
+
+export const SET_PAYMENT_METHOD_ON_CART = gql`
+  mutation SetPaymentMethodOnCart($input: SetPaymentMethodOnCartInput!) {
+    setPaymentMethodOnCart(input: $input) {
+      cart {
+        id
+        selected_payment_method {
+          code
+          title
+        }
+      }
+    }
+  }
+`;
+
+export const PLACE_ORDER = gql`
+  mutation PlaceOrder($input: PlaceOrderInput!) {
+    placeOrder(input: $input) {
+      order {
+        order_number
+        client_secret
+      }
+      orderV2 {
+        order_number
+      }
+      errors {
+        message
+        code
+      }
+    }
+  }
+`;
+
+export const CREATE_CHECKOUT_SESSION = gql`
+  query CreateCheckoutSession($input: CreateCheckoutSessionInput!) {
+    createCheckoutSession(input: $input) {
+      checkout_url
+      session_id
+      price_id
+      amount
+      currency
+      product_id
+      billing_period
+    }
+  }
+`;
+
+export const CANCEL_SUBSCRIPTION = gql`
+  mutation CancelSubscription($input: CancelSubscriptionInput!) {
+    cancelSubscription(input: $input)
+  }
+`;
+
+export const GET_SAVED_PAYMENT_METHODS = gql`
+  query GetSavedPaymentMethods {
+    getSavedPaymentMethods {
+      id
+      type
+      card {
+        brand
+        last4
+        exp_month
+        exp_year
+      }
+    }
+  }
+`;
+
+export const CHANGE_PAYMENT_METHOD = gql`
+  mutation ChangePaymentMethod($input: ChangePaymentMethodInput!) {
+    changePaymentMethod(input: $input) {
+      id
+      status
+      plan_name
+      current_period_start
+      current_period_end
+      amount
+      currency
+      billing_period
+      cancel_at_period_end
+      payment_method {
+        id
+        type
+        card {
+          brand
+          last4
+          exp_month
+          exp_year
+        }
+      }
+    }
+  }
+`;
+
+export const UPGRADE_SUBSCRIPTION = gql`
+  mutation UpgradeSubscription($input: UpgradeSubscriptionInput!) {
+    upgradeSubscription(input: $input) {
+      checkout_url
+      session_id
+      price_id
+      amount
+      currency
+      product_id
+      billing_period
+    }
+  }
+`;
